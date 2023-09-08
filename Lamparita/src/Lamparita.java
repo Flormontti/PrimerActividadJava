18c25e0a897e82d99ec42d197940a1bf644bd805
Flormontti <flor.montti@gmail.com>
2023-09-08 17:56:25 -0300

    PrimerActividadJava

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,21 @@
 public class Lamparita {
-    public static void main(String[] args) throws Exception {
+
+    private boolean estado;
+
+    public void encender() {
+        this.estado = true;
+        System.out.println("La Lamparita esta encendida");
+    }
+
+    public void apagar() {
+        this.estado = false;
+        System.out.println("La Lamparita esta apagada");
+    }
+
+    public boolean estado() {
+        return estado;
+    }
+    public static void main(String[] args) {
         System.out.println("Vamos a programar una lamparita! ");
     }
 }
